18f5e8c3a17989a2b34e8e98540c1cd58f9359f8
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2022-07-23 23:46:54 +0530

    Update main.cpp

--- DSA Practice Sheet/Linked List/Multiply Two Numbers Represented by Linked Lists/main.cpp
+++ DSA Practice Sheet/Linked List/Multiply Two Numbers Represented by Linked Lists/main.cpp
@@ -19,5 +19,21 @@ and returns their product*/
 /*You are required to complete this method*/
 long long  multiplyTwoLists (Node* l1, Node* l2)
 {
+    long long n = 1000000007;
+    long long num1 = 0, num2 = 0;
+    while (l1 ||  l2)
+    {
+        if(l1) 
+        {
+            num1 = ((num1)*10)%n + l1->data;
+            l1 = l1->next;
+        }
+        if(l2)
+        {
+            num2 = ((num2)*10)%n + l2->data;
+            l2 = l2->next
+        }
+    }
+    return ((num1%n)*(num2%n))%n;
     
 }
