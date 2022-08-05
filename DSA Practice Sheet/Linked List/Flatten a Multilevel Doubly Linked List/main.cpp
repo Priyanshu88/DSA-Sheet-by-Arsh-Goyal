95f896235a71b47fcd836e86aadcc1ceb6750307
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2022-08-05 16:48:06 +0530

    commit

--- DSA Practice Sheet/Linked List/Flatten a Multilevel Doubly Linked List/main.cpp
+++ DSA Practice Sheet/Linked List/Flatten a Multilevel Doubly Linked List/main.cpp
@@ -9,10 +9,96 @@ public:
 };
 */
 
+// start from head node move one step each time to the next node
+// when meet with a node with child as p follow its child chain to the end and connect the tail node with p->next,
+// by doing this we merged the child chain back to the main thread
+// return to p and proceed until find next node with child
+// repeat until reach null
+
 class Solution
 {
 public:
     Node *flatten(Node *head)
     {
+
+        // base case
+
+        if (head == NULL)
+            return head;
+
+        // p pointer pointing on head
+
+        Node *p = head;
+
+        // traversing p until it does not become null
+
+        while (p != NULL)
+        {
+
+            // if there is no child proceed
+
+            if (p->child == NULL)
+            {
+                p = p->next;
+                continue;
+            }
+
+            
+            // if child found finding the tail of child and linking to the p->next
+            
+            Node *temp = p->child;
+
+            // finding tail of the child
+
+            while (temp->next != NULL)
+            {
+
+                // connecting tail with p next, if it is not null
+
+                temp = temp->next;
+            }
+            temp->next = p->next;
+            if (p->next != NULL)
+                p->next->prev = temp;
+
+            // connecting p with p child and removing p child
+            
+            p->next = p->child;
+            p->child->prev = p;
+            p->child = NULL;
+        }
+        return head;
     }
 };
+
+// T.C.:O(n)
+// S.C.:O(1)
+
+
+// 
+
+class Solution
+{
+public:
+    Node *flatten(Node *head)
+    {
+        for (Node *h = head; h; h = h->next)
+        {
+            if(h->child)
+            {
+                Node *next = h->next;
+                h->next = h->child;
+                h->next->prev = h;
+                h->child = NULL;
+                Node *p = h->next;
+                while(p->next)
+                    p = p->next;
+                p->next = next;
+                if(next)
+                    next->prev = p;
+            }
+        }
+        return head;
+    }
+};
+
--- DSA Practice Sheet/Linked List/Remove Nth Node from End of List/main.cpp
+++ DSA Practice Sheet/Linked List/Remove Nth Node from End of List/main.cpp
@@ -13,6 +13,48 @@ class Solution
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
+
+        // creating a new dummy node
+
+        ListNode *start = new ListNode();
+
+        // connecting the start next with the head node of linked list
+
+        start->next = head;
+
+        // making point slow and fast on start
+
+        ListNode *slow = start;
+        ListNode *fast = start;
+
+        // traversing the list from head node to nth node from starting by moving fast pointer one step ahead
+
+        for (int i = 1; i <= n;i++)
+            fast = fast->next;
+
+        // traversing until fast next does not become null
         
+        while(fast->next != NULL)
+        {
+
+            // fast will move from nth index until fast next does not become null
+
+            fast = fast->next;
+
+            // slow will be moving one step ahead from starting and will point on nth node that has to be removed
+
+            slow = slow->next;
+        }
+
+        // now slow next will be slow next next as we are detaching the link to remove th nth node
+
+        slow->next = slow->next->next;
+
+        // return head node as start next
+
+        return start->next;
     }
 };
+
+// T.C.:O(n)
+// S.C.:O(1)
