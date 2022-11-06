09bba9c5e8c5912eecb9dc0c892d7c911407745b
PRIYANSHU PANDEY <2040020@sliet.ac.in>
2022-11-06 12:37:03 +0530

    commit

--- DSA Practice Sheet/Strings/Minimum Window Substring/main.cpp
+++ DSA Practice Sheet/Strings/Minimum Window Substring/main.cpp
@@ -1,9 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
 
     // sliding window technique 
     // storing all characters from string t to an unordered map mp.
-    // ans is to store the minimum size of substring start is to store the start index and count is to store the mao size
+    // ans is to store the minimum size of substring start is to store the start index and count is to store the map size
     // two pointers i and j for iteration j will iterate and will decrement count element in map
     // if value becomes 0 means we got all the elements of that char till so decrement size of count
     // when the count will be 0 if there is a substring with all elements present
